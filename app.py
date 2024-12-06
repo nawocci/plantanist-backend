3e9229b43ef03cb60cb7bf42e8852e4393479aa1
Naufal Altaf <me@altaf.xyz>
2024-12-06 15:35:02 +0700

    Listen to $PORT before defaulting to 8080

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
-# placeholder route to / for testing which return hello world
-@app.route('/')
-def hello():
+@app.route('/', methods=['POST'])
+def hello_world():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
